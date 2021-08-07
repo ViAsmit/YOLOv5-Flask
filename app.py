b9ce09e8d607b3802d52dd81ef09fda44a313deb
ViAsmit <asmit.vml.962@gmail.com>
2021-08-07 21:59:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from re import DEBUG, sub
+from flask import Flask, render_template, request, redirect, send_file, url_for
+from werkzeug.utils import secure_filename, send_from_directory
+import os
+import subprocess
+
+app = Flask(__name__)
+
+uploads_dir = os.path.join(app.instance_path, 'uploads')
+
+os.makedirs(uploads_dir, exist_ok=True)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route("/detect", methods=['POST'])
+def detect():
+    if not request.method == "POST":
+        return
+    video = request.files['video']
+    video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
+    print(video)
+    subprocess.run("ls")
+    subprocess.run(['python3', '../yolov5/detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))])
+
+    # return os.path.join(uploads_dir, secure_filename(video.filename))
+    obj = secure_filename(video.filename)
+    return obj
+
+@app.route('/return-files', methods=['GET'])
+def return_file():
+    obj = request.args.get('obj')
+    loc = os.path.join("runs/detect", obj)
+    print(loc)
+    try:
+        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+        # return send_from_directory(loc, obj)
+    except Exception as e:
+        return str(e)
+
+# @app.route('/display/<filename>')
+# def display_video(filename):
+# 	#print('display_video filename: ' + filename)
+# 	return redirect(url_for('static/video_1.mp4', code=200))
---
+++ static/index.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+  $("#sendbutton").click(() => {
+    imagebox = $("#imagebox");
+    link = $("#link");
+    input = $("#imageinput")[0];
+    if (input.files && input.files[0]) {
+      let formData = new FormData();
+      formData.append("video", input.files[0]);
+      $.ajax({
+        url: "/detect", // fix this to your liking
+        type: "POST",
+        data: formData,
+        cache: false,
+        processData: false,
+        contentType: false,
+        error: function (data) {
+          console.log("upload error", data);
+          console.log(data.getAllResponseHeaders());
+        },
+        success: function (data) {
+          console.log(data);
+          // bytestring = data["status"];
+          // image = bytestring.split("'")[1];
+          $("#link").css("visibility", "visible");
+          $("#download").attr("href", "static/" + data);
+          console.log(data);
+        },
+      });
+    }
+  });
+};
+
+function readUrl(input) {
+  imagebox = $("#imagebox");
+  console.log(imagebox);
+  console.log("evoked readUrl");
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      console.log(e.target);
+
+      imagebox.attr("src", e.target.result);
+      //   imagebox.height(500);
+      //   imagebox.width(800);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
